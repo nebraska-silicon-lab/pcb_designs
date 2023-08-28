faafa994cde2937fd4be06372e27b605143d9996
Caleb Fangmeier <caleb@fangmeier.tech>
2023-08-28 17:30:46 -0500

    Adds Stencil design for TFPX 1x2 CROC modules

--- Tools/gerber-to-kicad-footprint/main.py
+++ Tools/gerber-to-kicad-footprint/main.py
@@ -4,12 +4,31 @@ from gerber.primitives import Region, Rectangle, Line
 from pathlib import Path
 
 
-def convert(gerber_filename):
+def convert(gerber_filename, to_mm=False):
     gerber_path = Path(gerber_filename)
     gerber_file = gerber.read(gerber_path)
 
     # Find just the SMD pads
     smd_pads = [p for p in gerber_file.primitives if isinstance(p, Rectangle)]
+
+    class FauxPad:
+        def __init__(self, region):
+            self._region = region
+
+        @property
+        def position(self):
+            return (0.5 * (self._region.bounding_box[0][0] + self._region.bounding_box[0][1]),
+                    0.5 * (self._region.bounding_box[1][0] + self._region.bounding_box[1][1]))
+
+        @property
+        def width(self):
+            return self._region.bounding_box[0][1] - self._region.bounding_box[0][0]
+
+        @property
+        def height(self):
+            return self._region.bounding_box[1][1] - self._region.bounding_box[1][0]
+
+    smd_pads.extend([FauxPad(p) for p in gerber_file.primitives if isinstance(p, Region)])
     smd_pads.sort(key=lambda p: p.position[0])
 
     ss_lines = []
@@ -26,27 +45,32 @@ def convert(gerber_filename):
     top = min(pad.position[1]-0.5*pad.height for pad in smd_pads)
     bottom = max(pad.position[1]+0.5*pad.height for pad in smd_pads)
 
-    cx = 25.4*0.5*(left + right)
-    cy = 25.4*0.5*(top + bottom)
+    if to_mm:
+        unit_factor = 25.4
+    else:
+        unit_factor = 1.0
+
+    cx = unit_factor*0.5*(left + right)
+    cy = unit_factor*0.5*(top + bottom)
 
     with open(gerber_path.with_suffix('.kicad_mod'), 'w') as f:
         f.write(f'(module {gerber_path.stem} (layer F.Cu) (tedit 612FE60C)\n')
 
         for idx, pad in enumerate(smd_pads):
-            x, y = 25.4*pad.position[0], 25.4*pad.position[1]
-            w, h = 25.4*pad.width, 25.4*pad.height
-            f.write(f"  (pad {idx+1} smd rect (at {x-cx} {-1*(y-cy)}) (size {w} {h}) (layers F.Cu F.Mask)\n"
-                    # f"    (solder_mask_margin 0.15)\n"
+            x, y = unit_factor*pad.position[0], unit_factor*pad.position[1]
+            w, h = unit_factor*pad.width, unit_factor*pad.height
+            f.write(f"  (pad {idx+1} smd rect (at {x-cx} {-1*(y-cy)}) (size {w} {h}) (layers F.Cu F.Mask F.Paste)\n"
+                    f"    (solder_mask_margin 0.0)\n"
                     ")\n"
                     )
 
-        for line in ss_lines:
-            sx, sy = 25.4 * line[0][0], 25.4 * line[0][1]
-            ex, ey = 25.4 * line[1][0], 25.4 * line[1][1]
-            w = 25.4 * line[2]
-            f.write(f"  (fp_line (start {sx-cx} {-1*(sy-cy)}) (end {ex-cx} {-1*(ey-cy)}) "
-                    f"(layer F.SilkS) (width {w}))\n"
-                    )
+        # for line in ss_lines:
+        #     sx, sy = unit_factor * line[0][0], unit_factor * line[0][1]
+        #     ex, ey = unit_factor * line[1][0], unit_factor * line[1][1]
+        #     w = unit_factor * line[2]
+        #     f.write(f"  (fp_line (start {sx-cx} {-1*(sy-cy)}) (end {ex-cx} {-1*(ey-cy)}) "
+        #             f"(layer F.SilkS) (width {w}))\n"
+        #             )
 
         f.write(')\n')
 
@@ -56,7 +80,12 @@ def convert(gerber_filename):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('gerber_file')
-    args = parser.parse_args()
-    convert(args.gerber_file)
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+
+    Tk().withdraw()
+    filename = askopenfilename()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('gerber_file')
+    # args = parser.parse_args()
+    convert(filename)
